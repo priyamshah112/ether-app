24a32c73f67f50d4a704727eba08ce676e2017b5
ankit <ankitchiplunkar@gmail.com>
2018-03-02 15:30:08 +0100

    Handled error cases

--- analyseether/__init__.py
+++ analyseether/__init__.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, render_template
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 import os
@@ -8,3 +8,18 @@ app = Flask(__name__)
 app.config.from_object(os.environ['APP_SETTINGS'])
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
+
+
+@app.errorhandler(403)
+def forbidden_page(error):
+    return render_template("errors/403.html"), 403
+
+
+@app.errorhandler(404)
+def page_not_found(error):
+    return render_template("errors/404.html"), 404
+
+
+@app.errorhandler(500)
+def server_error_page(error):
+    return render_template("errors/500.html"), 500
