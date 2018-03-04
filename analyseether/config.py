465ebc767605cc9e25c5f41c0a99b8eab5af8aa1
ankit <ankitchiplunkar@gmail.com>
2018-03-04 08:54:30 +0100

    Removed csrf encoding

--- analyseether/config.py
+++ analyseether/config.py
@@ -7,18 +7,16 @@ class ProductionConfig(object):
     # Configurations for development enviornment
     DEBUG = True
     TESTING = False
-    CSRF_ENABLED = True
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     SECRET_KEY = os.environ.get('SECRET_KEY')
-    SQLALCHEMY_DATABASE_URI = os.environ['SQLALCHEMY_PROD_DATABASE_URI']
-    SECURITY_PASSWORD_SALT = os.environ['SECURITY_PASSWORD_SALT']
+    SQLALCHEMY_DATABASE_URI = os.environ.get(['SQLALCHEMY_PROD_DATABASE_URI'])
+    SECURITY_PASSWORD_SALT = os.environ.get(['SECURITY_PASSWORD_SALT'])
 
 
 class StagingConfig(object):
     # Configurations for staging enviornment
     DEBUG = True
     TESTING = True
-    CSRF_ENABLED = True
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     SECRET_KEY = os.environ.get('SECRET_KEY')
     SQLALCHEMY_DATABASE_URI = os.environ['SQLALCHEMY_STAGE_DATABASE_URI']
