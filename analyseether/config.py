645d0d81d41c4792cb7ebc0b28c078a905804eca
ankit <ankitchiplunkar@gmail.com>
2018-03-05 15:21:32 +0100

    added the details of mail sending

--- analyseether/config.py
+++ analyseether/config.py
@@ -12,6 +12,15 @@ class ProductionConfig(object):
     SQLALCHEMY_DATABASE_URI = os.environ['SQLALCHEMY_PROD_DATABASE_URI']
     SECURITY_PASSWORD_SALT = os.environ['SECURITY_PASSWORD_SALT']
 
+    # mail settings
+    MAIL_SERVER = os.environ['MAIL_SERVER']
+    MAIL_PORT = os.environ['MAIL_PORT']
+    MAIL_USE_TLS = os.environ['MAIL_USE_TLS']
+    MAIL_USE_SSL = os.environ['MAIL_USE_SSL']
+    MAIL_USERNAME = os.environ['MAIL_USERNAME']
+    MAIL_PASSWORD = os.environ['MAIL_PASSWORD']
+    # mail accounts
+    MAIL_DEFAULT_SENDER = os.environ['MAIL_DEFAULT_SENDER']
 
 class StagingConfig(object):
     # Configurations for staging enviornment
@@ -21,3 +30,13 @@ class StagingConfig(object):
     SECRET_KEY = os.environ.get('SECRET_KEY')
     SQLALCHEMY_DATABASE_URI = os.environ['SQLALCHEMY_STAGE_DATABASE_URI']
     SECURITY_PASSWORD_SALT = os.environ['SECURITY_PASSWORD_SALT']
+
+    # mail settings
+    MAIL_SERVER = os.environ['MAIL_SERVER']
+    MAIL_PORT = os.environ['MAIL_PORT']
+    MAIL_USE_TLS = os.environ['MAIL_USE_TLS']
+    MAIL_USE_SSL = os.environ['MAIL_USE_SSL']
+    MAIL_USERNAME = os.environ['MAIL_USERNAME']
+    MAIL_PASSWORD = os.environ['MAIL_PASSWORD']
+    # mail accounts
+    MAIL_DEFAULT_SENDER = os.environ['MAIL_DEFAULT_SENDER']
