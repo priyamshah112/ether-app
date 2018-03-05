065e8efa94dc402bcfe1b78553f2b4a6560e42f6
ankit <ankitchiplunkar@gmail.com>
2018-03-05 10:25:01 +0100

    imported views

--- analyseether/__init__.py
+++ analyseether/__init__.py
@@ -3,13 +3,17 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 import os
 
-
+# Initializing database and flask app
 app = Flask(__name__)
 app.config.from_object(os.environ['APP_SETTINGS'])
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
 
+# Importing views
+import analyseether.views
 
+
+# Error handlers
 @app.errorhandler(403)
 def forbidden_page(error):
     return render_template("errors/403.html"), 403
