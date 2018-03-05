ab58cadc888adf7bfd35c8ca2d75bcaa52eb154b
ankit <ankitchiplunkar@gmail.com>
2018-03-05 15:21:19 +0100

    added support for sending email

--- analyseether/__init__.py
+++ analyseether/__init__.py
@@ -1,13 +1,17 @@
-from flask import Flask, render_template
+from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
+from flask_mail import Mail
 import os
 
 # Initializing database and flask app
 app = Flask(__name__)
 app.config.from_object(os.environ['APP_SETTINGS'])
+
+# Extensions
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
+mail = Mail(app)
 
 # Importing views
 import analyseether.views
