985fbd69dd3d5a4c35a5e7948d9c2df74b6573f8
ankit <ankitchiplunkar@gmail.com>
2018-03-05 10:46:39 +0100

    removed error handlers

--- analyseether/__init__.py
+++ analyseether/__init__.py
@@ -11,19 +11,3 @@ migrate = Migrate(app, db)
 
 # Importing views
 import analyseether.views
-
-
-# Error handlers
-@app.errorhandler(403)
-def forbidden_page(error):
-    return render_template("errors/403.html"), 403
-
-
-@app.errorhandler(404)
-def page_not_found(error):
-    return render_template("errors/404.html"), 404
-
-
-@app.errorhandler(500)
-def server_error_page(error):
-    return render_template("errors/500.html"), 500
