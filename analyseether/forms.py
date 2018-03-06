be3b8be538bf77ec4a949c13486ee00b35c6d8ba
ankit <ankitchiplunkar@gmail.com>
2018-03-06 21:42:51 +0100

    changed email to string field

--- analyseether/forms.py
+++ analyseether/forms.py
@@ -1,10 +1,10 @@
 from flask_wtf import FlaskForm
 from wtforms.validators import InputRequired, Email
-from wtforms.fields.html5 import EmailField
+from wtforms import StringField
 
 
 class Form(FlaskForm):
-    email = EmailField('Email',
+    email = StringField('Email',
                     validators=[InputRequired("Please enter your email address."),
-                    Email("Please enter proper email address.")],
+                                Email("Please enter proper email address.")],
                     render_kw={"placeholder": "Please Enter Email Address"})
