4c9484c83db048bbc0c3a6dee02626ed6c6beec5
ankit <ankitchiplunkar@gmail.com>
2018-03-06 11:08:51 +0100

    removed the bug where I forgot to add receipents of email

--- analyseether/email.py
+++ analyseether/email.py
@@ -5,6 +5,7 @@ from analyseether import app, mailgun
 def send_email(to, subject, template):
     message = Message()
     message.subject = subject
+    message.recipients = [to]
     message.sender = app.config['MAIL_DEFAULT_SENDER']
     message.html = template
 
