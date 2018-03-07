be2e5d62ed8f08237b1fbbe67ff5976a16a8b8e1
ankit <ankitchiplunkar@gmail.com>
2018-03-07 09:24:43 +0100

    modified the text in email form

--- analyseether/forms.py
+++ analyseether/forms.py
@@ -5,6 +5,6 @@ from wtforms import StringField
 
 class Form(FlaskForm):
     email = StringField('Email',
-                    validators=[InputRequired("Please enter your email address."),
-                                Email("Please enter proper email address.")],
-                    render_kw={"placeholder": "Please Enter Email Address"})
+                    validators=[InputRequired("Please enter a non-empty email address."),
+                                Email("Please enter a proper email address.")],
+                    render_kw={"placeholder": "Enter your email and subscribe to our newsletter"})
