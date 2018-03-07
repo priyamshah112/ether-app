0181c340b4427d601a82571463d0e2583460ab98
ankit <ankitchiplunkar@gmail.com>
2018-03-07 09:25:21 +0100

    Made the token expiration time a variable, and providing that value in email

--- analyseether/token.py
+++ analyseether/token.py
@@ -1,13 +1,15 @@
 from itsdangerous import URLSafeTimedSerializer
 from analyseether import app
 
+TOKEN_EXPIRATION_SECONDS = 3600
+
 
 def generate_confirmation_token(email):
     serializer = URLSafeTimedSerializer(app.config['SECRET_KEY'])
     return serializer.dumps(email, salt=app.config['SECURITY_PASSWORD_SALT'])
 
 
-def confirm_token(token, expiration=3600):
+def confirm_token(token, expiration=TOKEN_EXPIRATION_SECONDS):
     serializer = URLSafeTimedSerializer(app.config['SECRET_KEY'])
     try:
         email = serializer.loads(
